3f53ac670d093612c5a4232d4571fda79eea2753
Adauto Júnior <adautodcjunior@gmail.com>
2024-01-05 14:56:37 -0300

    feat: finished lesson 01 of the "Doctrine: Migrations, relatórios e performance" course

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Alura\Doctrine\Helper\EntityManagerCreator;
-
-require_once 'vendor/autoload.php';
-
-$entityManager = EntityManagerCreator::createEntityManager();
-
-var_dump($entityManager);
