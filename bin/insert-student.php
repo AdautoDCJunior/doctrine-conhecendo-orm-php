253fab34bea4eb5edc26f5f45f5db09294d735ab
Adauto Júnior <adautodcjunior@gmail.com>
2024-01-05 16:20:52 -0300

    Aula 02 do curso "Doctrine: Migrations, relatórios e performance"

--- bin/insert-student.php
+++ bin/insert-student.php
@@ -8,25 +8,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $entityManager = EntityManagerCreator::createEntityManager();
 
-$student1 = new Student('Primeiro Aluno', new DateTime('1998-06-23'));
-$student1->addPhone(new Phone('(11)11111-1111'));
-$student1->addPhone(new Phone('(22)22222-2222'));
+$student = new Student(
+    $argv[1],
+    (assert($argv[2]) ? new DateTime($argv[2]) : null)
+);
 
-$student2 = new Student('Segundo Aluno', new DateTime('1998-06-23'));
-$student2->addPhone(new Phone('(33)33333-3333'));
-$student2->addPhone(new Phone('(44)44444-4444'));
-$student2->addPhone(new Phone('(55)55555-5555'));
+for ($i = 3; $i < $argc; $i++) {
+    $student->addPhone(new Phone($argv[$i]));
+}
 
-$student3 = new Student('Terceiro Aluno', new DateTime('1998-06-23'));
-$student3->addPhone(new Phone('(66)66666-6666'));
-
-$student4 = new Student('Quarto Aluno', new DateTime('1998-06-23'));
-$student4->addPhone(new Phone('(77)77777-7777'));
-$student4->addPhone(new Phone('(88)88888-8888'));
-$student4->addPhone(new Phone('(99)99999-9999'));
-
-$entityManager->persist($student1);
-$entityManager->persist($student2);
-$entityManager->persist($student3);
-$entityManager->persist($student4);
+$entityManager->persist($student);
 $entityManager->flush();
